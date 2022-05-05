497d4aeb1e244dd920cec1419e37b5ce531ad1e5
Mohammed Aman Khan <45879026+Mohammed-Aman-Khan@users.noreply.github.com>
2022-05-05 16:07:58 +0530

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
                 >
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248849.5522500068!2d77.49085545800294!3d12.954294593216147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1651743367139!5m2!1sen!2sin"
-                        allowfullscreen=""
+                        allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                         style={{
